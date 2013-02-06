b9ea16860acc325d383a9a526a525c7f30ce5984
Chip Rosenthal <chip@unicom.com>
2013-02-06 10:11:48 -0600

    Trying a different interface for FormGenerator. Laid down some fields on the form.

--- lib/tecfiler/form-generator.rb
+++ lib/tecfiler/form-generator.rb
@@ -4,50 +4,70 @@ require "tempfile"
 module TECFiler
   class FormGenerator
     
-    BASEDIR_FORMS = File.dirname(__FILE__) + "/forms"      
+    BASEDIR_FORMS = File.dirname(__FILE__) + "/forms"
     
-    def initialize(id, options = {})
-      @id = id
-      case id
-      when :COH
-        @authority = options[:authority] ||= "state.tx.us"
-        @version = options[:version] ||= "20110928"
-        @filename = "coh.pdf"
-        @produce_report = :produce_report_coh_20110928
-      when :SPAC
-        @authority = options[:authority] ||= "state.tx.us"
-        @version = options[:version] ||= "20110928"
-        @filename = "spac.pdf"
-        @produce_report = :produce_report_spac_20110928
-      else
-        raise "unknown form id \"#{id}\""
-      end
-      @template = "#{BASEDIR_FORMS}/#{@authority}/#{@version}/#{@filename}" 
-      raise "source file \"#{@template}\" not found" unless File.exist?(@template)
-    end
-    
-    def produce_report(entity)
-      self.send(@produce_report, entity)
-    end
-    
-    
-    def produce_report_coh_20110928(entity)
-      raise "data entity is not a COH" unless entity.instance_of?(TECFiler::Model::COH)
-      raise "form version mismatch (expected #{@version}, got #{entity.version})" unless entity.version == @version
+    def self.produce(entity, options = {})
       
-      #require "pp" ; pp entity, entity.contributions, entity.expenditures
+      @entity = entity
+      @authority = options[:authority] || "state.tx.us"
+      
+      case entity
+      when TECFiler::Model::COH
+        @id = :COH
+        @version = options[:version] || entity.version
+        @filename = "coh.pdf"
+        @produce = "produce_coh_#{@version}".to_sym
+      when TECFiler::Model::SPAC
+        @id = :SPAC
+        @version = options[:version] || entity.version
+        @filename = "spac.pdf"
+        @produce = ":produce_spac_#{@version}".to_sym
+      else
+        raise "do not know how to produce form for entity type #{entity.class}"
+      end      
+ 
+      @template = "#{BASEDIR_FORMS}/#{@authority}/#{@version}/#{@filename}"
+      raise "form template not found: #{@template}" unless File.exist?(@template)
+      
+      self.send(@produce)
+    end
+    
+    
+    def self.produce_coh_20110928
+      
+      #require "pp" ; pp @entity, @entity.contributions, @entity.expenditures
       
       fn = "/tmp/tec-form-xxxxxx.pdf"
       
       Prawn::Document.generate(fn) do |pdf|
-        pdf.start_new_page :template => @template, :template_page => 1     
-        pdf.text_box "Hello world", :at => [100, 100] 
+        #pdf.font_size 20
+        
+        def pdf.t(value, options)
+          return if value.empty? 
+          self.text_box value, options
+        end
+        
+        pdf.start_new_page :template => @template, :template_page => 1
+          
+        pdf.t @entity.coh_name_prefix, :at => [108, 618]
+        pdf.t @entity.coh_name_first, :at => [208, 618]
+        pdf.t @entity.coh_name_mi, :at => [350, 618]
+        pdf.t @entity.coh_name_nick, :at => [108, 585]
+        pdf.t @entity.coh_name_last, :at => [208, 585]
+        pdf.t @entity.coh_name_suffix, :at => [350, 585]
+        
+        pdf.t @entity.coh_address.join("\n"), :at => [108, 548]
+        
+        pdf.t "X", :at => [10, 520] #  if @entity.coh_address_changed
+          
+#property :coh_phone, String, :required => true, :format => :telno
+
       end
       
       return fn
     end
     
-    def produce_report_spac_20110928(entity)
+    def self.produce_spac_20110928
       raise "sorry, not implemented yet"
     end
     
--- test/src/test-form-generator.rb
+++ test/src/test-form-generator.rb
@@ -1,4 +1,5 @@
-$:.insert(0, "#{File.dirname(__FILE__)}/../../lib")
+cwd = File.dirname(__FILE__)
+$:.insert(0, "#{cwd}/../lib", "#{cwd}/../../lib")
 require "minitest/autorun"
 require "tecfiler"
 require "test-support"
@@ -8,40 +9,25 @@ class TestFormGenerator< MiniTest::Unit::TestCase
   # XXX - This is depending on other tests having run and created database.
   # Would be better if we create some mock data structures and use that.
   
-  def test01_new
-    g = TECFiler::FormGenerator.new(:COH)
-    refute_nil g
-    assert_instance_of TECFiler::FormGenerator, g
-    assert_equal :produce_report_coh_20110928, g.instance_variable_get(:@produce_report)
-    assert_equal "#{TECFiler::FormGenerator::BASEDIR_FORMS}/state.tx.us/20110928/coh.pdf", g.instance_variable_get(:@template)
-  end
-  
-  def test02_produce_report_fail_bad_entity
-    g = TECFiler::FormGenerator.new(:COH)
+  def test02_produce_fail_bad_entity
     e = assert_raises RuntimeError do
-      g.produce_report(Object.new)
+      TECFiler::FormGenerator::produce(Object.new)
     end
-    assert_equal "data entity is not a COH", e.message
+    assert_equal "do not know how to produce form for entity type Object", e.message
   end
   
-  def test03_produce_report_fail_bad_version
-    coh = TECFiler::Model::COH[1]    
+  def test03_produce_fail_bad_version
+    coh = TECFiler::Model::COH.new
     refute_nil coh
-    coh.version = "314159"
-    g = TECFiler::FormGenerator.new(:COH)
     e = assert_raises RuntimeError do
-      g.produce_report(coh)
+      TECFiler::FormGenerator::produce(coh, :version => "314159")
     end
-    assert_equal "form version mismatch (expected 20110928, got 314159)", e.message
+    assert_equal "form template not found: /home/chip/Workspace/tecfiler/lib/tecfiler/forms/state.tx.us/314159/coh.pdf", e.message
   end
   
-  def test02_report_coh
-    coh = TECFiler::Model::COH[1]
-    refute_nil coh
-    g = TECFiler::FormGenerator.new(:COH)
-    fn = g.produce_report(coh)
-    assert_match /\.pdf$/, fn
-    assert File.exist?(fn)
+  def test10_report_coh
+    coh = TECFiler::Model::COH.new(TECFiler::Test::PARAMS_COH)
+    fn = TECFiler::FormGenerator::produce(coh)
     $stderr.puts "Form has been dumped to: #{fn}"
   end  
   
