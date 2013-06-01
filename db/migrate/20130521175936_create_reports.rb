efe47f149b60de088584f4b9dbc02f99b773e475
Chip Rosenthal <chip@unicom.com>
2013-06-01 13:31:02 -0500

    Added fields needed for C/OH report.

--- db/migrate/20130521175936_create_reports.rb
+++ db/migrate/20130521175936_create_reports.rb
@@ -1,17 +1,62 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-
+      
       t.integer :filer_id
-      t.string :status
-      t.string :office_held
-      t.string :office_sought
-      t.string :report_type
-      t.string :status
+      
+      ###
+      #
+      # Fields below are filled in from the Filer
+      #
+
+      t.string :coh_name_prefix
+      t.string :coh_name_first
+      t.string :coh_name_mi
+      t.string :coh_name_nick
+      t.string :coh_name_last
+      t.string :coh_name_suffix
+      
+      t.string :coh_address_street
+      t.string :coh_address_suite
+      t.string :coh_address_city
+      t.string :coh_address_state
+      t.string :coh_address_zip
+      t.boolean :coh_address_changed   
+         
+      t.string :coh_phone
+      
+      #####
+      #
+      # Fields below are filled in from the Treasurer
+      #
+      
+      t.string :treasurer_name_prefix
+      t.string :treasurer_name_first
+      t.string :treasurer_name_mi
+      t.string :treasurer_name_nick
+      t.string :treasurer_name_last
+      t.string :treasurer_name_suffix
+      
+      t.string :treasurer_address_street
+      t.string :treasurer_address_suite
+      t.string :treasurer_address_city
+      t.string :treasurer_address_state
+      t.string :treasurer_address_zip
+      t.boolean :treasurer_address_changed     
+       
+      t.string :treasurer_phone
+      
+      #####
+      #
+      # Other report values
+      #
+      
+      t.string :status # placeholder for workflow (e.g. draft, filed, accepted, deleted?)
+      t.string :report_type # Enum[:JAN15, :JUL15, :ELECTION_30DAY, :ELECTION_8DAY, :RUNNOFF, :EXCEED_500, :TREASURER_APPT, :FINAL]
       t.date :period_begin
       t.date :period_end
       t.date :election_date
-      t.string :election_type
+      t.string :election_type # Enum[:PRIMARY, :RUNOFF, :GENERAL, :SPECIAL]
       t.string :office_held
       t.string :office_sought
 
@@ -27,6 +72,7 @@ class CreateReports < ActiveRecord::Migration
 
       t.timestamps
     end
+        
     add_index :reports, :filer_id
   end
 
